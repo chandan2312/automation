b7e841bf2f658db119268a67b1f5933cfb67760d
chandan2312 <chandanchaudhari2312@gmail.com>
2024-07-14 20:02:41 +0530

    exp

--- couponverse/pm2.sh
+++ couponverse/pm2.sh
@@ -24,29 +24,26 @@ for script in "${scripts[@]}"; do
 
     while true; do
         # Start the script
-        $PM2_PATH start /var/www/dc_factory/xvfb.sh --name "$name" -- /var/www/dc_factory/$script_path $country "$iter" "$key"  --no-autorestart
+        $PM2_PATH start /var/www/dc_factory/xvfb.sh --name "$name" -- /var/www/dc_factory/$script_path $country "$iter" "$key" --no-autorestart
 
-        # Output logs in the background
-        $PM2_PATH logs "$name" &
-
-        # Wait until the process is no longer running
+        # Monitor the process
         while true; do
             status=$($PM2_PATH describe "$name" | grep "status" | awk '{print $2}')
-            echo "Current status of $name: $status" 
+            echo "Current status of $name: $status"
+
             if [ "$status" != "online" ]; then
-                $PM2_PATH logs "$name" --lines 15
                 log_output=$($PM2_PATH logs "$name" --lines 15)
                 echo "$log_output"
 
-                if echo "$log_output" | grep -q "Script ended"; then
+                if echo "$log_output" | grep -qi "Script ended"; then
                     echo "$name completed successfully"
                     break 2  # Exit both loops and move to the next script
                 fi
 
-                if echo "$log_output" | grep -q "too many requests"; then
+                if echo "$log_output" | grep -qi "too many requests"; then
                     echo "$name $country $iter $key - 🗝️ key error 🗝️"
                     key=$((key % key_range + 1))
-                elif echo "$log_output" | grep -q "Navigation timeout\|partial translation\|status code 500"; then
+                elif echo "$log_output" | grep -qi "Navigation timeout\|partial translation\|status code 500\|Fatal server\|Make sure an X server"; then
                     echo "$name $country $iter $key - ⏭️ ⏭️"
                     iter=$((iter + 1))
                 else
@@ -55,9 +52,14 @@ for script in "${scripts[@]}"; do
 
                 $PM2_PATH delete "$name"
                 echo "Restarting $name $country $iter $key"
-                break
+                sleep 60  # Sleep only after status is not online
+                break  # Exit inner loop to restart the script
             fi
-            sleep 30  # Check every 30 seconds
         done
+
+        # Ensure the outer loop breaks if "Script ended" was found
+        if echo "$log_output" | grep -qi "Script ended"; then
+            break  # Exit outer loop to move to the next script
+        fi
     done
 done
