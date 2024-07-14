bf7938a1d4aeff78ba4956755b0fa295f15b538e
chandan2312 <chandanchaudhari2312@gmail.com>
2024-07-14 13:13:42 +0530

    exp

--- couponverse/pm2.sh
+++ couponverse/pm2.sh
@@ -6,7 +6,6 @@ scripts=(
     "cron/network/promocodie.js promocodie_CZ cz 0"
     "cron/network/promocodie.js promocodie_DE de 0"
     "cron/network/promocodie.js promocodie_BR br 0"
-    
 )
 
 key=1
@@ -24,29 +23,32 @@ for script in "${scripts[@]}"; do
 
     while true; do
         $PM2_PATH start /var/www/dc_factory/xvfb.sh --name "$name" -- /var/www/dc_factory/$script_path $country "$iter" "$key" --interpreter /bin/bash --no-autorestart
-        $PM2_PATH logs "$name"
 
-        # Check the last 15 lines of the logs for "Ended successfully"
+        $PM2_PATH logs "$name" &
+
+        $PM2_PATH wait "$name"
+
         log_output=$($PM2_PATH logs "$name" --lines 15)
+        echo "$log_output" 
+
         if echo "$log_output" | grep -q "Ended successfully"; then
             echo "$name completed successfully"
-            break  # Exit the while loop and move to the next script
-        else
-            error_message=$(echo "$log_output" | grep -E "${error_messages["too_many_requests"]}|${error_messages["navigation_error"]}|${error_messages["partial_translation"]}")
-
-            if echo "$error_message" | grep -q "${error_messages["too_many_requests"]}"; then
-                echo "$name $country $iter $key - 🗝️ key error 🗝️"
-                echo "Error : $error_message"
-                key=$((key % key_range + 1))
-            else
-                echo "$name $country $iter $key - ⚠️ unknown error ⚠️"
-                echo "Error : $error_message"
-                iter=$((iter + 1))
-            fi
-
-            $PM2_PATH delete "$name"
-            echo "Restarting $name $country $iter $key"
+            break  
         fi
-        sleep 5
+
+        error_message=$(echo "$log_output" | grep -E "${error_messages["too_many_requests"]}|${error_messages["navigation_error"]}|${error_messages["partial_translation"]}")
+
+        if echo "$error_message" | grep -q "${error_messages["too_many_requests"]}"; then
+            echo "$name $country $iter $key - 🗝️ key error 🗝️"
+            echo "Error : $error_message"
+            key=$((key % key_range + 1))
+        else
+            echo "$name $country $iter $key - ⚠️ unknown error ⚠️"
+            echo "Error : $error_message"
+            iter=$((iter + 1))
+        fi
+
+        $PM2_PATH delete "$name"
+        echo "Restarting $name $country $iter $key"
     done
 done
