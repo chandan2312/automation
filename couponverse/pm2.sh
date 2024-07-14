9727ddcf755bc7ab4cfbae008a7bfb30087e1679
chandan2312 <chandanchaudhari2312@gmail.com>
2024-07-14 12:53:48 +0530

    exp

--- couponverse/pm2.sh
+++ couponverse/pm2.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+PM2_PATH=$(which pm2)  # Get the full path to pm2
+
 scripts=(
     "cron/network/asion.p.js couponasion_ID id 0"
     "cron/network/asion.p.js couponasion_RU ru 0"
@@ -14,14 +18,14 @@ for script in "${scripts[@]}"; do
     country=$3
     iter=$4
 
-    pm2 stop "$name"
+    $PM2_PATH stop "$name"
 
     while true; do
-        pm2 start /var/www/dc_factory/xvfb.sh --name "$name" -- /var/www/dc_factory/$script_path $country "$iter" "$key" --interpreter /bin/bash
-        pm2 wait "$name"
+        $PM2_PATH start /var/www/dc_factory/xvfb.sh --name "$name" -- /var/www/dc_factory/$script_path $country "$iter" "$key" --interpreter /bin/bash
+        $PM2_PATH wait "$name"
 
         # Check the last 15 lines of the logs for "Ended successfully"
-        log_output=$(pm2 logs "$name" --lines 15)
+        log_output=$($PM2_PATH logs "$name" --lines 15)
         if echo "$log_output" | grep -q "Ended successfully"; then
             echo "$name completed successfully"
             break  # Exit the while loop and move to the next script
@@ -32,17 +36,13 @@ for script in "${scripts[@]}"; do
                 echo "$name $country $iter $key - 🗝️ key error 🗝️"
                 echo "Error : $error_message"
                 key=$((key % key_range + 1))
-            elif echo "$error_message" | grep -q "${error_messages["navigation_error"]}\|${error_messages["partial_translation"]}"; then
-                echo "$name $country $iter $key - ⏭️ skip error ⏭️"
-                echo "Error : $error_message"
-                iter=$((iter + 1))
             else
                 echo "$name $country $iter $key - ⚠️ unknown error ⚠️"
                 echo "Error : $error_message"
                 iter=$((iter + 1))
             fi
 
-            pm2 delete "$name"
+            $PM2_PATH delete "$name"
             echo "Restarting $name $country $iter $key"
         fi
         sleep 5
